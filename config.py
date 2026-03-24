03f8a29e72f87458ebae327daa35d6da0d6cc632
settapudichamathan-lang <settapudichamathan@gmail.com>
2026-03-24 11:03:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8762503654:AAG1p5BtLIdZl4rYCEaOPdxh3zujf_kvOo4")
+API_ID = int(os.environ.get("API_ID", "@TamilFileSharingBot"))
+API_HASH = os.environ.get("API_HASH", "naruto bot")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "6299192020"))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "Tecfreaktamil")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6123706931"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://<naruto>:<mathanuzumaki995106>@cluster0.nezofsj.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Mathanuzumaki")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003094784222"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003855296564"))
 
-FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1003056865925"))
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002420958043"))
 
 FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "0"))
 
